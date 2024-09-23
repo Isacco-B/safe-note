f25d9c14d60b0db95eac28c37648a99ea8548ee6
Isacco-B <git@isaccobertoli.com>
2024-09-23 21:33:41 +0200

    add routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,18 @@
-import { Button } from "./components/ui/button";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import NoteDetail from "./pages/NoteDetail";
+import NotFound from "./pages/NotFound";
 
 function App() {
-  return <Button>Click me</Button>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/note/:id" element={<NoteDetail />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
